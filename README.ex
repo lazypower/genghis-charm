5866842afa3675ae9d41fe4e0a39fec58c075729
Charles Butler <chuck@dasroot.net>
2013-09-29 11:06:02 -0400

    Imports charm at rev 5

---
+++ README.ex
@@ -0,0 +1,27 @@
+Overview
+--------
+
+This charm provides genghisapp from http://genghisapp.com/
+
+Genghis is a single file MongoDB Admin app. The application itself provides a dynamic on the fly connection to your MongoDB server/shard/replicaset defined by the user. All connection details are stored in browser local storage. 
+
+Usage
+-----
+
+Step by step instructions on using the charm:
+
+    juju deploy genghisapp
+
+
+You can then browse to http://ip-address to configure the service. 
+
+Configuration
+-------------
+
+Port - define the port Genghisapp will receive connections on.
+
+Contact Information
+-------------------
+Author: Charles Butler <chuck@dasroot.net>
+Report bugs at: http://github.com/chuckbutler/genghis-charm/issues 
+
